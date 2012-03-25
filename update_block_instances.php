f6382ca25b8e45d9e18a58df9fb66de727dd7437
Nathan Kowald <nathankowald@gmail.com>
2012-03-25 19:11:01 +0100

    Some base64_encoded URLs live in the block_pinned table to searching in here now also

--- update_block_instances.php
+++ update_block_instances.php
@@ -1,74 +1,73 @@
 <?php
-	
+
 require_once('config.php');
 require_login(); 
 
 if (!isadmin()) {
-    error("Only the administrator can access this page!", $CFG->wwwroot);
+    error('Only the administrator can access this page!', $CFG->wwwroot);
 }
 
+/* ---------------------- Settings ------------------------ */
+    $search    = 'domain.com/VLE/';
+    $replace   = 'vle.domain.com';
 
-/* -------------------- Settings ---------------------- */
-$search    = 'domain.com/vle';
-$replace   = 'vle.domain.com';
-
-$case_sensitive = false; // false = Match 'vle' and 'VLE'
-/* ---------------------------------------------------- */
+    $case_sensitive = false; // false = Match 'vle' and 'VLE'
+/* -------------------------------------------------------- */
 
 
-// Find non-empty block instances
-if ($records = get_records_select('block_instance', 'configdata != "" AND configdata != "Tjs="')) {
+$records1 = get_records_select('block_instance', 'configdata != "" AND configdata != "Tjs="');
+$records2 = get_records_select('block_pinned', 'configdata != "" AND configdata != "Tjs="');
+$records  = array_merge($records1, $records2);
+
+if (count($records > 0)) {
 
     echo "<h2>Replacing '<span style=\"color:red;\">$search</span>' with '<span style=\"color:green;\">$replace</span>' inside block content.</h2>";
-
-    $no_found = count($records);
-    echo "<h3>$no_found non-empty block instances found</h3>";
-    
-    $c = 1;
-    $found_count = 0;
-    
+    echo "<h3>".count($records)." non-empty block instances found</h3>";
     echo '<pre>';
+
+    $found_count = 0;
+    $c = 1;
     foreach ($records as $record) {
         
-        echo "<p><strong>Block $c</strong> - ";
+        $result = "<p><strong>Block $c</strong> - ";
 
         $configdata = unserialize(base64_decode($record->configdata));
         if ($configdata->text !== NULL) {
         
-            // Convert HTML tags to character entities to allow URL replacement inside links
+            // Allows URL replacement inside links
             $html = htmlspecialchars($configdata->text);
             $string_found = ($case_sensitive) ? strpos($html, $search) : stripos($html, $search);
+
+            if ($string_found !== false) {
             
-            if ($string_found === true) {
-            
-                $updated_content = ($case_sensitive) ? htmlspecialchars_decode(str_replace($search, $replace, $html)) : htmlspecialchars_decode(str_ireplace($search, $replace, $html));
-                $configdata->text = $updated_content;
-                // New configdata
-                $new_content = base64_encode(serialize($configdata));
-                $record->configdata = $new_content;
+                $configdata->text = ($case_sensitive) ?
+                    htmlspecialchars_decode(str_replace($search, $replace, $html)) : 
+                    htmlspecialchars_decode(str_ireplace($search, $replace, $html));
+
+                $record->configdata = base64_encode(serialize($configdata));
                 
-                if (update_record('block_instance', $record)) {
-                    echo '<strong style="color:green;">Found and replaced!</strong>';
+                $table = (isset($record->page_id)) ? 'block_instance' : 'block_pinned';
+                if (update_record($table, $record)) {
+                    $result .= '<strong style="color:green;">Found and replaced!</strong>';
                     $found_count++;
                 } else {
-                    echo 'FAILed to update block_instance :*(';
+                    $result .= 'FAILed to update block_instance :*(';
                 }
                 
             } else {
-                echo "Search string not found";
+                $result .= 'Search string not found';
             }
             
         } else {
-            // Nothing inside block's content area.
-            echo 'No content';
+            $result .= 'No content';
+
         }
+        echo $result . '</p>';
         $c++;
         
-        echo '<p>';
-        
     }
+
     echo '</pre>';
-    
     echo "<h3>$found_count blocks updated to use the string '$replace' instead of '$search'.</h3>";
 
 } else {
